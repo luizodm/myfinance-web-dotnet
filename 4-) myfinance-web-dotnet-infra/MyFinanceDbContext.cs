0a1b7c65bc500ff4e3e9b14d90b06f21351b8e66
luizodm <luizd.avila@hotmail.com>
2025-11-03 10:34:55 -0300

    .

---
+++ 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain.Entities;
+
+namespace myfinance_web_dotnet_infra;
+
+public class MyFinanceDbContext : DbContext
+{
+    public DbSet<PlanoConta> PlanoConta { get; set; }
+    public DbSet<Transacao> Transacao { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False;");
+        optionsBuilder.UseSqlServer(@"Server=meuservidorsqlserver.database.windows.net;Database=myfinance;User Id=user;Password=myF1n4nc3;");
+    }
+}
